4bf3c95680bccbe5de1b92a7a81e4372a9c83a99
1dgidi <douglasgidi1@gmail.com>
2022-06-20 18:09:31 +0100

    made home page look better

--- Investigete_Enron_web/bend/views.py
+++ Investigete_Enron_web/bend/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django import forms
+from django.forms import ModelForm, Textarea
+
+from .models import EmailBody
+
+
+class EForm(ModelForm):
+    class Meta:
+        model = EmailBody
+        fields = ['body_text']
+
 
 def home(request):
-    context = {}
-    return render(request, 'bend/home.html', context)
+    if request.POST:
+        form = EForm(request.POST)
+        if form.is_valid():
+            email_body = form.cleaned_data['body_text']
+            new_req = EmailBody(body_text=email_body)
+            new_req.save()
+            return redirect('bend:result', new_req.id)
+    return render(request, 'bend/home.html', {'form': EForm()})
 
 def about(request):
     pass
 
 def contact(request):
+    pass
+
+def result(request, pk):
     pass
 
