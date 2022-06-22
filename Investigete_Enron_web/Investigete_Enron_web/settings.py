0a25b6f8281a8c918f946baac940feeb6a2ec97d
1dgidi <douglasgidi1@gmail.com>
2022-06-22 04:04:25 +0100

    Revert "Automatic configuration with django-heroku"
    This reverts commit 9753ee9757ddb772b7ec2a662fc39dae201ef7f9.

--- Investigete_Enron_web/Investigete_Enron_web/settings.py
+++ Investigete_Enron_web/Investigete_Enron_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-irja3@6^pl!dsr#bo+32-*u!&93sls#(q*0b15j6*k*vv6%nz@'
+SECRET_KEY = 'django-insecure-irja3@6^pl!dsr#bo+32-*u!&93sls#(q*0b15j6*k*vv6%nz@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
